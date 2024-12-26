f4e94b925816ee058c4fbfb3e89e2108e862c2f3
Arsenii <arspopov01@gmail.com>
2024-12-26 12:19:37 +0100

    feat: add solutions file-names, transform-array

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const nameCount = new Map();
+  const result = [];
+
+  for (let name of names) {
+    if (!nameCount.has(name)) {
+      result.push(name);
+      nameCount.set(name, 1);
+    } else {
+      let count = nameCount.get(name);
+      let newName = `${name}(${count})`;
+
+      nameCount.set(name, count + 1);
+
+      while (nameCount.has(newName)) {
+        count++;
+        newName = `${name}(${count})`;
+      }
+
+      result.push(newName);
+      nameCount.set(newName, 1);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  renameFiles
+  renameFiles,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,64 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const result = [];
+  const discardIndices = new Set();
+
+  for (let i = 0; i < arr.length; i++) {
+    if (discardIndices.has(i)) {
+      continue;
+    }
+
+    switch (arr[i]) {
+      case "--discard-next":
+        if (i + 1 < arr.length) {
+          discardIndices.add(i + 1);
+        }
+        break;
+
+      case "--discard-prev":
+        if (i - 1 >= 0 && !discardIndices.has(i - 1)) {
+          result.pop();
+        }
+        break;
+
+      case "--double-next":
+        if (i + 1 < arr.length) {
+          result.push(arr[i + 1]);
+        }
+        break;
+
+      case "--double-prev":
+        if (i - 1 >= 0 && !discardIndices.has(i - 1)) {
+          result.push(arr[i - 1]);
+        }
+        break;
+
+      default:
+        result.push(arr[i]);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
